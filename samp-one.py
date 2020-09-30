c2258bc24a27b317040922750a000653b6723597
Emmanuel Achana <achanamosey@gmail.com>
2020-09-30 22:16:14 +0000

    Updated the file.

--- samp-one.py
+++ samp-one.py
@@ -4,26 +4,28 @@ def generate_number():
     return f'+233{rd.choice(chce)}{rd.randint(1, 100000000)}'
 
 def remove_duplicates(data):
-    temp = []
-    for x in range(len(data)):
-        if data[x] not in temp:
-            temp.append(data[x])
-    return temp
+    res = [] 
+    for i in range(len(data)): 
+        if data[i] not in res:
+            res.append(data[i]) 
+    return res
+
+# def remove_duplicates(dict_array):
+#     return [i for n, i in enumerate(dict_array) if i not in dict_array[n + 1:]]
 
 data = [
-    {'name': 'Emmanuel Achana', 'age': 22, 'level': 5, 'phone': generate_number()},
-    {'name': 'Peter Scott', 'age': 18, 'level': 2, 'phone': generate_number()},
-    {'name': 'Frank Castle', 'age': 20, 'level': 5, 'phone': generate_number()},
-    {'name': 'Stephen Strange', 'age': 27, 'level': 3, 'phone': generate_number()},
-    {'name': 'Steve Rogers', 'age': 25, 'level': 4, 'phone': generate_number()},
-
-    {'name': 'Emmanuel Achana', 'age': 22, 'level': 5, 'phone': generate_number()},
-    {'name': 'Peter Scott', 'age': 18, 'level': 2, 'phone': generate_number()},
-    {'name': 'Frank Castle', 'age': 20, 'level': 5, 'phone': generate_number()},
-    {'name': 'Stephen Strange', 'age': 27, 'level': 3, 'phone': generate_number()},
-    {'name': 'Steve Rogers', 'age': 25, 'level': 4, 'phone': generate_number()}
+    {'name': 'Emmanuel Achana', 'age': 22, 'level': 5, 'phone': '+233566831631'}, 
+    {'name': 'Peter Scott', 'age': 18, 'level': 2, 'phone': '+233536496890'}, 
+    {'name': 'Frank Castle', 'age': 20, 'level': 5, 'phone': '+233253006512'}, 
+    {'name': 'Stephen Strange', 'age': 27, 'level': 3, 'phone': '+233546939676'}, 
+    {'name': 'Steve Rogers', 'age': 25, 'level': 4, 'phone': '+233528052634'}, 
+    {'name': 'Emmanuel Achana', 'age': 22, 'level': 5, 'phone': '+233566831631'}, 
+    {'name': 'Peter Scott', 'age': 18, 'level': 2, 'phone': '+233536496890'}, 
+    {'name': 'Frank Castle', 'age': 20, 'level': 5, 'phone': '+233253006512'}, 
+    {'name': 'Stephen Strange', 'age': 27, 'level': 3, 'phone': '+233546939676'}, 
+    {'name': 'Steve Rogers', 'age': 25, 'level': 4, 'phone': '+233528052634'}
 ]
 
+print('=====' * 40)
 print(remove_duplicates(data))
-print('=====' * 30)
-print(data)
+print('=====' * 40)
