d20461f8871a193182fbc296dfe32ad7be37b45c
Emmanuel M. Achana <achanamosey@gmail.com>
2021-05-15 01:06:30 +0100

    Updated the gam controller.

--- sample-api-service-with-sqlite/game_controller.py
+++ sample-api-service-with-sqlite/game_controller.py
@@ -34,10 +34,18 @@ def get_by_id(id):
     cursor.execute(statement, [id])
     return cursor.fetchone()
 
+'''
+for row in cur.execute('SELECT * FROM stocks ORDER BY price'):
+        print(row)
 
+'''
 def get_games():
+    temp = []
     db = get_db()
     cursor = db.cursor()
     query = "SELECT id, name, price, rate FROM games"
-    cursor.execute(query)
-    return cursor.fetchall()
+    for row in cursor.execute(query):
+        data = {'id': row[0], 'name': row[1], 'price': row[2], 'rate': row[3]}
+        temp.append(data)
+    #cursor.execute(query)
+    return temp #cursor.fetchall()
