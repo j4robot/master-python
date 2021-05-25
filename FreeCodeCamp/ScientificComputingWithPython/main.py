e18e4fb15b639beef67727e13a0e993f22923cfc
Emmanuel M. Achana <achanamosey@gmail.com>
2021-05-25 23:16:51 +0100

    Added brackets to group code.

--- FreeCodeCamp/ScientificComputingWithPython/main.py
+++ FreeCodeCamp/ScientificComputingWithPython/main.py
@@ -1,6 +1,6 @@
 #This is a course on Scientific Computing Using Python 
 raw = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-array = [x ** 2 for x in raw ]
+array = [(x ** 2) for x in raw ]
 
 print(raw)
 print(array)
