2a36e5c5300844eadb2914cff6c11e5bd5aa0da7
Emmanuel M. Achana <achanamosey@gmail.com>
2021-06-01 15:15:29 +0100

    Get today's date.

--- FreeCodeCamp/ScientificComputingWithPython/dict.py
+++ FreeCodeCamp/ScientificComputingWithPython/dict.py
@@ -1,3 +1,5 @@
 import datetime
 
-dict_one = {'name': 'Emmanuel Moses Achana', 'age': 26, 'code_langs': ['JavaScript', 'Java', 'C#', 'Python'], 'database': ['MySQL', 'MongoDB', 'MSSQL'], 'date': datetime.date}
+dict_one = {'name': 'Emmanuel Moses Achana', 'age': 26, 'code_langs': ['JavaScript', 'Java', 'C#', 'Python'], 'database': ['MySQL', 'MongoDB', 'MSSQL'], 'date': datetime.date.today()}
+
+print(dict_one)
