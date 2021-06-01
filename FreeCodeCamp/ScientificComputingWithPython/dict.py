656b84b267e789d03dead01cc0c5c67b2bffe6d4
Emmanuel M. Achana <achanamosey@gmail.com>
2021-06-01 15:11:32 +0100

    Imported datetime

--- FreeCodeCamp/ScientificComputingWithPython/dict.py
+++ FreeCodeCamp/ScientificComputingWithPython/dict.py
@@ -1,1 +1,3 @@
-dict_one = {'name': 'Emmanuel Moses Achana', 'age': 26, 'code_langs': ['JavaScript', 'Java', 'C#', 'Python'], 'database': ['MySQL', 'MongoDB', 'MSSQL']}
+import datetime
+
+dict_one = {'name': 'Emmanuel Moses Achana', 'age': 26, 'code_langs': ['JavaScript', 'Java', 'C#', 'Python'], 'database': ['MySQL', 'MongoDB', 'MSSQL'], 'date': datetime.date}
