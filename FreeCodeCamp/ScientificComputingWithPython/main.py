42542827472613aecc423389157e4bd9cdc2d3ac
Emmanuel M. Achana <achanamosey@gmail.com>
2021-06-01 14:50:51 +0100

    Changed the positions of the code.

--- FreeCodeCamp/ScientificComputingWithPython/main.py
+++ FreeCodeCamp/ScientificComputingWithPython/main.py
@@ -1,10 +1,10 @@
 #This is a course on Scientific Computing Using Python 
-raw = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-array = [(x ** 2) for x in raw ]
 
-print(raw)
+array = [(x ** 2) for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ]
 print(array)
 
-raw_ = [1, 2, 1, 3, 2, 1, 4, 5]
+raw = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(raw)
 
+raw_ = [1, 2, 1, 3, 2, 1, 4, 5]
 print(set(raw_))
