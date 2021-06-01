6190e9908b13f1b50ea61a77cd69940d2567ca05
Emmanuel M. Achana <achanamosey@gmail.com>
2021-06-01 15:27:55 +0100

    Removed a line of code.

--- FreeCodeCamp/ScientificComputingWithPython/main.py
+++ FreeCodeCamp/ScientificComputingWithPython/main.py
@@ -3,8 +3,5 @@
 array = [(x ** 2) for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ]
 print(array)
 
-raw = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(raw)
-
-raw_ = [1, 2, 1, 3, 2, 1, 4, 5]
-print(set(raw_))
+raw = [1, 2, 1, 3, 2, 1, 4, 5]
+print(set(raw))
