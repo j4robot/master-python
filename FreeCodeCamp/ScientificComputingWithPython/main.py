238b585781ef3afc6f1325a1a475ea0c5ac7596d
Emmanuel M. Achana <achanamosey@gmail.com>
2021-06-01 18:31:41 +0100

    Created function to return largest number from three params

--- FreeCodeCamp/ScientificComputingWithPython/main.py
+++ FreeCodeCamp/ScientificComputingWithPython/main.py
@@ -1,7 +1,19 @@
 #This is a course on Scientific Computing Using Python 
 
 array = [(x ** 2) for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ]
-print(array)
+#print(array)
 
 raw = [1, 2, 1, 3, 2, 1, 4, 5]
-print(set(raw))
+#print(set(raw))
+
+def max_num(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+
+
+
+print(max_num(5, 1, 3))
